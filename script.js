82dc4760c7ea91066dc6f3e9fea5492347d6fe3b
¯¯̿̿¯̿̿'̿̿̿̿̿̿̿'̿̿'̿̿̿̿̿'̿̿̿)͇̿̿)̿̿̿̿ '̿̿̿̿̿̿\̵͇̿̿\=(•̪̀●́)=o/̵͇̿̿/'̿̿ ̿ ̿̿ <shane-white@outlook.de>
2022-09-20 03:34:11 +0200

    commit

--- script.js
+++ script.js
@@ -1,138 +1,129 @@
-import { white, g10, g100 } from '@carbon/themes';
-import {
-  teal20,
-  blue40,
-  magenta40,
-  purple40,
-  gray90,
-  gray100,
-  white0,
-} from '@carbon/elements';
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2019, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-const getTheme = (interiorTheme) => {
-  let theme = g10; // default
+import 'carbon-web-components/es/components/button/button.js';
+import 'carbon-web-components/es/components/form/form-item.js';
+import 'carbon-web-components/es/components/input/input.js';
+import 'carbon-web-components/es/components/notification/inline-notification.js';
+import 'carbon-web-components/es/components/inline-loading/index.js';
 
-  const isLightTheme = interiorTheme === 'white' || interiorTheme === 'g10';
-
-  if (interiorTheme === 'dark') {
-    theme = g100;
+const submit = async formData => {
+  // Simulates server latency
+  await new Promise(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, 500)
+  );
+  // In a real-word application, you can simply do:
+  // `await fetch('https://your.server/path/to/the/endpoint', { method: 'POST', body: formData })`
+  const username = formData.get('username');
+  if (!username) {
+    throw Object.assign(new Error('Login failed'), {
+      errors: {
+        username: 'User does not exist',
+      },
+    });
+  } else if (!['john', 'anne'].includes(username)) {
+    throw Object.assign(new Error('Login failed'), {
+      errors: {
+        username: 'Wrong user name (Has to be john or anne)',
+      },
+    });
+  } else if (formData.get('password') !== 'form') {
+    throw Object.assign(new Error('Login failed'), {
+      errors: {
+        password: 'Wrong password (Has to be the name of the parent directory of this example directory)',
+      },
+    });
+  } else {
+    const values = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of formData.entries()) {
+      values[key] = value;
+    }
+    alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`); // eslint-disable-line no-alert
   }
-
-  if (interiorTheme === 'white') {
-    theme = white;
-  }
-
-  theme = {
-    ...theme,
-    plainBackground: isLightTheme ? gray100 : gray90,
-    property: teal20,
-    tag: blue40,
-    important: blue40,
-    string: magenta40,
-    boolean: purple40,
-  };
-
-  return {
-    plain: {
-      backgroundColor: theme.plainBackground,
-      color: white0,
-    },
-    styles: [
-      {
-        types: ['comment', 'prolog', 'doctype', 'cdata'],
-        style: {
-          color: theme.text02,
-        },
-      },
-      {
-        types: ['namespace'],
-        style: {
-          opacity: 0.7,
-        },
-      },
-      {
-        types: ['tag', 'operator'],
-        style: {
-          color: theme.tag,
-        },
-      },
-      {
-        types: ['property', 'function', 'attr-name'],
-        style: {
-          color: theme.property,
-        },
-      },
-      {
-        types: ['variable'],
-        style: {
-          color: theme.inverse01, // white
-        },
-      },
-      {
-        types: ['string'],
-        style: {
-          color: theme.string,
-        },
-      },
-      {
-        types: ['entity'],
-        style: {
-          cursor: 'help',
-        },
-      },
-      {
-        types: [
-          'boolean',
-          'entity',
-          'url',
-          'attr-value',
-          'control',
-          'directive',
-          'unit',
-          'statement',
-          'regex',
-          'at-rule',
-          'selector',
-          'keyword',
-          'placeholder',
-        ],
-        style: {
-          color: theme.boolean,
-        },
-      },
-      {
-        types: ['deleted'],
-        style: {
-          textDecorationLine: 'line-through',
-        },
-      },
-      {
-        types: ['inserted'],
-        style: {
-          borderBottom: '1px dotted #c22dd5',
-          textDecorationLine: 'underline',
-        },
-      },
-      {
-        types: ['italic'],
-        style: {
-          fontStyle: 'italic',
-        },
-      },
-      {
-        types: ['important', 'bold'],
-        style: {
-          fontWeight: 'bold',
-        },
-      },
-      {
-        types: ['important'],
-        style: {
-          color: theme.important,
-        },
-      },
-    ],
-  };
 };
 
-export default getTheme;
+(() => {
+  const notification = document.getElementById('notification');
+  const form = document.getElementById('form-login');
+  const inputUsername = document.getElementById('input-username');
+  const inputPassword = document.getElementById('input-password');
+  const btnLogin = document.getElementById('btn-login');
+  const btnReset = document.getElementById('btn-reset');
+
+  let pristine;
+
+  notification.open = false;
+
+  const setPristine = value => {
+    pristine = value;
+    btnReset.disabled = Boolean(pristine);
+  };
+
+  const setSubmitting = value => {
+    inputUsername.disabled = value;
+    inputPassword.disabled = value;
+    btnLogin.disabled = value;
+    btnReset.disabled = value || pristine;
+  };
+
+  const setValidity = (errors = {}) => {
+    notification.open = 'username' in errors || 'password' in errors;
+    inputUsername.invalid = 'username' in errors;
+    inputUsername.validityMessage = errors.username;
+    inputPassword.invalid = 'password' in errors;
+    inputPassword.validityMessage = errors.password;
+  };
+
+  const login = async () => {
+    // Gathers the form data
+    // In modern browsers, `formdata` event is fired automatically upon `form.submit()`, etc.
+    const formData = new FormData(form);
+    const event = new CustomEvent('formdata', { bubbles: true, cancelable: false, composed: false });
+    event.formData = formData;
+    form.dispatchEvent(event);
+    // Submits the form
+    setSubmitting(true);
+    try {
+      await submit(formData);
+      setValidity();
+    } catch ({ errors }) {
+      setValidity(errors);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const reset = () => {
+    notification.open = false;
+    inputUsername.invalid = false;
+    inputUsername.value = '';
+    inputPassword.invalid = false;
+    inputPassword.value = '';
+    setPristine(true);
+  };
+
+  inputUsername.addEventListener('input', () => {
+    setPristine(false);
+  });
+
+  inputPassword.addEventListener('input', () => {
+    setPristine(false);
+  });
+
+  btnLogin.addEventListener('click', async () => {
+    login();
+  });
+
+  btnReset.addEventListener('click', () => {
+    reset();
+  });
+})();
